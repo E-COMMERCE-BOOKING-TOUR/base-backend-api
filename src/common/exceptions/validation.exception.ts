375f1678d95a6073808098adc85bf9ef31dfc30a
chinhdev <phamchinh347@gmail.com>
2025-11-06 00:19:55 +0700

    first commit

---
+++ src/common/exceptions/validation.exception.ts
@@ -0,0 +1,14 @@
+import { BadRequestException } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
+
+export class ValidationException extends BadRequestException {
+  constructor(public validationErrors: ValidationError[]) {
+    super({
+      message: 'Dữ liệu không hợp lệ',
+      errors: validationErrors.map((error) => ({
+        field: error.property,
+        issues: Object.values(error.constraints || {}),
+      })),
+    });
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ValidationException } from '@/common/exceptions/validation.exception';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig({ path: '.env.local' });
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory(errors) {
+        return new ValidationException(errors);
+      },
+    }),
+  );
+
+  if (process.env.ENV === 'local' || process.env.ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Booking Tour API')
+      .setDescription('Api for booking tour')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .addBearerAuth(
+        { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+        'refresh-token',
+      )
+      .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/v1/docs', app, documentFactory);
+  }
+
+  await app.listen(process.env.APP_PORT ?? 3000);
+}
+bootstrap();
